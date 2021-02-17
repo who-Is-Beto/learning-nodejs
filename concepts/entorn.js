6ad0bfbc97f996f142589a914e0179e8af422e25
Beto Cortés <roberto.cortes13233@gmail.com>
2021-02-17 13:49:11 -0600

    initial commit

---
+++ concepts/entorn.js
@@ -0,0 +1,6 @@
+let name = process.env.NAME || 'no name'
+let web = process.env.WEB || 'no web'
+
+console.log(`hello I am ${name} and my web is ${web}`)
+
+// HOW TO INSTALL NODEMON $npm i -g nodemon 
